3a574d5d76fcbd529688b3d5114716251ba25967
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2025-03-31 20:19:05 -0500

    commit 12: quick review.

--- tests.py
+++ tests.py
@@ -1,14 +1,10 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-to_find = 5
-found = False
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
 
-for i in range(len(my_list)):
-    found = my_list[i] == to_find
-    if found:
-        break
+for number in bets:
+    if number in drawn:
+        hits += 1
 
-if found:
-    print("Element found at index", i)
-else:
-    print("absent")
+print(hits)
 
