4060a67ced0903f70db73a733823f9bc92196f02
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2025-04-04 21:25:38 -0500

    commit 14: Testing for final.

--- tests.py
+++ tests.py
@@ -29,8 +29,55 @@
 # print(foo)
 
 
-my_list = [1, 2, 3]
-# Insert line of code here.
-print(foo)
+# my_list = [1, 2, 3]
+# # Insert line of code here.
+# print(foo)
+
+# try:
+#    # Code that may raise an exception
+#    risky_call()
+# except ValueError:
+#    print("Caught ValueError")
+# except TypeError:
+#    print("Caught TypeError")
+# except Exception:
+#    print("Caught a different exception")
+
+
+
+# #q8
+# try:
+#     raise Exception("An error occurred")
+# except ValueError as ve:
+#     print("ValueError:", str(ve))
+# except BaseException as be:
+#     print("BaseException:", str(be))
+# except:
+#     print("Caught some other exception")
+
+
+
+
+# def generate_pattern(n):
+#     def create_pattern():
+#         return '**' * n
+
+#     return create_pattern
+
+# pattern1 = generate_pattern(1)
+# pattern2 = generate_pattern(2)
+# print(pattern1() + pattern2())
+
+import os
+
+os.mkdir('pictures')
+os.chdir('pictures')
+
+print(os.getcwd())
+
+
+
+
+
 
 
